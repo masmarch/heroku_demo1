92c6e9da85763da45417466123ed5c672453ba70
March <pongsapak@fitwhey.com>
2021-06-28 21:52:38 +0700

    bulid datatbase

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express")
 const app = express()
+const db  = require("./db.js")
+
 const PORT = process.env.PORT || 8080
 
 app.get("/", (req, res) => {
-    res.json({reults: "ok", data:[1,2,2,3,4,5]})
+    // res.json({reults: "ok", data:[1,2,2,3,4,5]})
+    db.query("SELECT * FROM user WHERE user.email='cs27@fitwhey.com'",(err, result) => {
+        if (err){
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    });
+
 })
 
 app.listen(PORT, () => {
